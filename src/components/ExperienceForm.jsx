7a23391a54f9547cc60bc519a539f5aed3273f7d
asil <9745019+asiill@users.noreply.github.com>
2023-12-08 18:25:43 -0500

    Add Experience section

--- src/components/ExperienceForm.jsx
+++ src/components/ExperienceForm.jsx
@@ -1,3 +1,71 @@
 import React from "react";
 
-export default function ExperienceForm(){}
+export default function ExperienceForm({
+    employer, location, startDate, endDate, position, description, id, handleChange, handleDelete,
+}) {
+    return (
+        <form className="experience-form">
+            <button className={id} onClick={handleDelete}>Delete</button>
+            <label className="form-label">Employer</label>
+            <input
+                type="text"
+                placeholder="XYZ Inc."
+                name="employer"
+                className={id}
+                value={employer}
+                onChange={handleChange}
+                required
+            />
+            <label className="form-label">Location</label>
+            <input
+                type="text"
+                placeholder="Montreal, Quebec"
+                name="location"
+                className={id}
+                value={location}
+                onChange={handleChange}
+                required
+            />
+            <label className="form-label">Start date</label>
+            <input
+                type="text"
+                placeholder="2017"
+                name="startDate"
+                className={id}
+                value={startDate}
+                onChange={handleChange}
+                required
+            />
+            <label className="form-label">End date</label>
+            <input
+                type="text"
+                placeholder="Present"
+                name="endDate"
+                className={id}
+                value={endDate}
+                onChange={handleChange}
+                required
+            />
+            <label className="form-label">Position</label>
+            <input
+                type="text"
+                placeholder="Graphic designer"
+                name="position"
+                className={id}
+                value={position}
+                onChange={handleChange}
+                required
+            />
+            <label className="form-label">Description</label>
+            <textarea
+                type="text"
+                placeholder="Please describe your position in detail..."
+                name="description"
+                className={id}
+                value={description}
+                onChange={handleChange}
+                required
+            ></textarea>
+        </form>
+    )
+}

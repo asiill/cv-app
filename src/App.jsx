1359483f295f66e0ce149452ec780e4058630f95
asil <9745019+asiill@users.noreply.github.com>
2023-12-16 22:48:25 -0500

    Add print button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import { useReactToPrint } from 'react-to-print';
 import uniqid from "uniqid";
 import CVForm from "./components/CVForm.jsx";
 import CVPreview from "./components/CVPreview.jsx";
@@ -8,132 +9,147 @@ export default function App() {
     const [editMode, setEditMode] = useState(true);
 
     const [personalInfo, setPersonalInfo] = useState({
-        name: "", email: "", phone: "", location: "",
+      name: "", email: "", phone: "", location: "",
     });
 
     const [educationInfo, setEducationInfo] = useState({
-        educationArr: [{school: "", location: "", degree: "", date: "", id: uniqid()}],
+      educationArr: [{school: "", location: "", degree: "", date: "", id: uniqid()}],
     });
 
     const [experienceInfo, setExperienceInfo] = useState({
-        experienceArr: [{employer: "", location: "", startDate: "", endDate: "", position: "", description: "", id: uniqid()}],
+      experienceArr: [{employer: "", location: "", startDate: "", endDate: "", position: "", description: "", id: uniqid()}],
     });
 
     const handlePersonalChange = (e) => {
-        setPersonalInfo({ ...personalInfo, [e.target.name]: e.target.value });
+      setPersonalInfo({ ...personalInfo, [e.target.name]: e.target.value });
     };
 
     const handleEducationChange = (e) => {
-        setEducationInfo({
-            educationArr: educationInfo.educationArr.map(edu => {
-                return (
-                    edu.id === e.target.className ? { ...edu, [e.target.name]: e.target.value } : edu
-                );
-            }),
-        });
+      setEducationInfo({
+        educationArr: educationInfo.educationArr.map(edu => {
+            return (
+                edu.id === e.target.className ? { ...edu, [e.target.name]: e.target.value } : edu
+            );
+        }),
+      });
     };
 
     const handleExperienceChange = (e) => {
-        setExperienceInfo({
-            experienceArr: experienceInfo.experienceArr.map(exp => {
-                return (
-                    exp.id === e.target.className ? { ...exp, [e.target.name]: e.target.value } : exp
-                );
-            }),
-        });
+      setExperienceInfo({
+        experienceArr: experienceInfo.experienceArr.map(exp => {
+            return (
+                exp.id === e.target.className ? { ...exp, [e.target.name]: e.target.value } : exp
+            );
+        }),
+      });
     };
 
     const handleEducationDelete = (e) => {
-        setEducationInfo({
-            educationArr: educationInfo.educationArr.filter(edu => {
-                return (
-                    edu.id !== e.target.className
-                );
-            }),
-        });
+      setEducationInfo({
+        educationArr: educationInfo.educationArr.filter(edu => {
+            return (
+                edu.id !== e.target.className
+            );
+        }),
+      });
     };
 
     const handleExperienceDelete = (e) => {
-        setExperienceInfo({
-            experienceArr: experienceInfo.experienceArr.filter(ex => {
-                return (
-                    ex.id !== e.target.className
-                );
-            }),
-        });
+      setExperienceInfo({
+        experienceArr: experienceInfo.experienceArr.filter(ex => {
+            return (
+                ex.id !== e.target.className
+            );
+        }),
+      });
     };
 
     const handleAddNewEducation = () => {
-        setEducationInfo({
-            educationArr: educationInfo.educationArr.concat({
-                school: "",
-                location: "",
-                degree: "",
-                date: "",
-                id: uniqid(),
-            }),
-        });
+      setEducationInfo({
+        educationArr: educationInfo.educationArr.concat({
+            school: "",
+            location: "",
+            degree: "",
+            date: "",
+            id: uniqid(),
+        }),
+      });
     };
 
     const handleAddNewExperience = () => {
-        setExperienceInfo({
-            experienceArr: experienceInfo.experienceArr.concat({
-                employer: "",
-                location: "",
-                startDate: "",
-                endDate: "",
-                position: "",
-                description: "",
-                id: uniqid(),
-            }),
-        });
+      setExperienceInfo({
+        experienceArr: experienceInfo.experienceArr.concat({
+          employer: "",
+          location: "",
+          startDate: "",
+          endDate: "",
+          position: "",
+          description: "",
+          id: uniqid(),
+        }),
+      });
     };
 
     const handleKeyDown = (e) => {
-        if (e.keyCode === 13) {
-            e.preventDefault();
-        }
+      if (e.keyCode === 13) {
+        e.preventDefault();
+      }
     };
 
     const handleShowPreview = () => {
-        setEditMode(false);
+      setEditMode(false);
     };
 
     const handleEdit = () => {
-        setEditMode(true);
+      setEditMode(true);
     };
 
+    const componentRef = useRef();
+
+    const handlePrint = useReactToPrint({
+      content: () => componentRef.current,
+      documentTitle: "CV",
+    });
+
     const editContainer = (
-        <>
-            <CVForm
-                personalInfo={personalInfo}
-                educationInfo={educationInfo}
-                experienceInfo={experienceInfo}
-                handlePersonalChange={handlePersonalChange}
-                handleEducationChange={handleEducationChange}
-                handleExperienceChange={handleExperienceChange}
-                handleEducationDelete={handleEducationDelete}
-                handleExperienceDelete={handleExperienceDelete}
-                handleAddNewEducation={handleAddNewEducation}
-                handleAddNewExperience={handleAddNewExperience}
-                handleKeyDown={handleKeyDown}
-                handleShowPreview={handleShowPreview}
-            />
-        </>
+      <>
+        <CVForm
+          personalInfo={personalInfo}
+          educationInfo={educationInfo}
+          experienceInfo={experienceInfo}
+          handlePersonalChange={handlePersonalChange}
+          handleEducationChange={handleEducationChange}
+          handleExperienceChange={handleExperienceChange}
+          handleEducationDelete={handleEducationDelete}
+          handleExperienceDelete={handleExperienceDelete}
+          handleAddNewEducation={handleAddNewEducation}
+          handleAddNewExperience={handleAddNewExperience}
+          handleKeyDown={handleKeyDown}
+          handleShowPreview={handleShowPreview}
+        />
+      </>
     );
 
     const previewContainer = (
-        <>
-            <CVPreview
-                personalInfo={personalInfo}
-                educationInfo={educationInfo}
-                experienceInfo={experienceInfo}
-                handleEdit={handleEdit}
-            />
-        </>
+      <>
+        <div className="btn-container">
+          <button className="edit-btn" onClick={handleEdit}>
+            Edit
+          </button>
+          <button className="print-btn" onClick={handlePrint}>
+            Print
+          </button>
+        </div>
+        <CVPreview
+          ref={componentRef}
+          personalInfo={personalInfo}
+          educationInfo={educationInfo}
+          experienceInfo={experienceInfo}
+        />
+      </>
     );
 
     return (
-        editMode ? editContainer : previewContainer
+      editMode ? editContainer : previewContainer
     );
 }
--- src/components/CVPreview.jsx
+++ src/components/CVPreview.jsx
@@ -1,69 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
 import PersonalItem from "./PersonalItem.jsx";
 import EducationItem from "./EducationItem.jsx";
 import ExperienceItem from "./ExperienceItem.jsx";
 
-export default function CVPreview({
-    personalInfo,
-    educationInfo,
-    experienceInfo,
-    handleEdit,
-}) {
-    return (
-        <div className="cv-preview">
-            <div className="edit-btn-container">
-                <button className="edit-btn" onClick={handleEdit}>
-                    Edit
-                </button>
-            </div>
-            <div className="cv-preview-header">
-                <PersonalItem
-                    name={personalInfo.name}
-                    location={personalInfo.location}
-                    email={personalInfo.email}
-                    phone={personalInfo.phone}
-                />
-            </div>
-            <div className="cv-preview-main">
-                <div className="education-preview-container">
-                    <div className="section-header">
-                        <h1>Education</h1>
-                        <hr />
-                    </div>
-                    {educationInfo.educationArr.map(edu => {
-                        return (
-                            <EducationItem
-                                key={edu.id}
-                                school={edu.school}
-                                location={edu.location}
-                                degree={edu.degree}
-                                date={edu.date}
-                                id={edu.id}
-                            />
-                        );
-                    })}
+export default class CVPreview extends Component{
+        render() {
+        const { personalInfo, educationInfo, experienceInfo, } = this.props;
+
+        return (
+            <div className="cv-preview">
+                <div className="cv-preview-header">
+                    <PersonalItem
+                        name={personalInfo.name}
+                        location={personalInfo.location}
+                        email={personalInfo.email}
+                        phone={personalInfo.phone}
+                    />
                 </div>
-                <div className="experience-preview-container">
-                    <div className="section-header">
-                        <h1>Experience</h1>
-                        <hr />
+                <div className="cv-preview-main">
+                    <div className="education-preview-container">
+                        <div className="section-header">
+                            <h1>Education</h1>
+                            <hr />
+                        </div>
+                        {educationInfo.educationArr.map(edu => {
+                            return (
+                                <EducationItem
+                                    key={edu.id}
+                                    school={edu.school}
+                                    location={edu.location}
+                                    degree={edu.degree}
+                                    date={edu.date}
+                                    id={edu.id}
+                                />
+                            );
+                        })}
                     </div>
-                    {experienceInfo.experienceArr.map(exp => {
-                        return (
-                            <ExperienceItem
-                                key={exp.id}
-                                employer={exp.employer}
-                                location={exp.location}
-                                startDate={exp.startDate}
-                                endDate={exp.endDate}
-                                position={exp.position}
-                                description={exp.description}
-                                id={exp.id}
-                            />
-                        );
-                    })}
+                    <div className="experience-preview-container">
+                        <div className="section-header">
+                            <h1>Experience</h1>
+                            <hr />
+                        </div>
+                        {experienceInfo.experienceArr.map(exp => {
+                            return (
+                                <ExperienceItem
+                                    key={exp.id}
+                                    employer={exp.employer}
+                                    location={exp.location}
+                                    startDate={exp.startDate}
+                                    endDate={exp.endDate}
+                                    position={exp.position}
+                                    description={exp.description}
+                                    id={exp.id}
+                                />
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }

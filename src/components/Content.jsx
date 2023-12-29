103bc29e1cba974a7e76fae57ea0c7a673cae748
asil <9745019+asiill@users.noreply.github.com>
2023-12-28 23:44:08 -0500

    Modify buttons under header to include text

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -160,8 +160,14 @@ export default function Content() {
     const editContainer = (
       <main id="edit-container">
         <nav className="btn-container">
-          <button id="preview-btn" onClick={handleShowPreview}><img src={previewImg} title="Show preview" alt="" /></button>
-          <button id="example-btn" onClick={handleLoadExample}><img src={loadImg} title="Load example" alt="" /></button>
+          <button id="preview-btn" onClick={handleShowPreview}>
+            <img src={previewImg} alt="" />
+            <span>Preview</span>
+          </button>
+          <button id="example-btn" onClick={handleLoadExample}>
+            <img src={loadImg} alt="" />
+            <span>Example</span>
+          </button>
         </nav>
         <CVForm
           personalInfo={personalInfo}
@@ -187,8 +193,14 @@ export default function Content() {
     const previewContainer = (
       <main id="preview-container">
         <nav className="btn-container">
-          <button id="edit-btn" onClick={handleEdit}><img src={editImg} title="Edit CV" alt="Edit document" /></button>
-          <button id="print-btn" onClick={handlePrint}><img src={printImg} title="Print CV" alt="Print document" /></button>
+          <button id="edit-btn" onClick={handleEdit}>
+            <img src={editImg} alt="" />
+            <span>Edit</span>
+          </button>
+          <button id="print-btn" onClick={handlePrint}>
+            <img src={printImg} alt="" />
+            <span>Print</span>
+          </button>
         </nav>
         <CVPreview
           ref={componentRef}

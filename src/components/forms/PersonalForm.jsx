e89cbba5eda998c9fe58d2fc05c5b58b2f5f0122
asil <9745019+asiill@users.noreply.github.com>
2023-12-28 23:00:17 -0500

    Associate form labels with input

--- src/components/forms/PersonalForm.jsx
+++ src/components/forms/PersonalForm.jsx
@@ -12,59 +12,65 @@ export default function PersonalForm({
     return (
         <form className="personal-form">
             <div className="form-row">
-                <label>Full Name</label>
+                <label for="name">Full Name</label>
                 <input
                     type="text"
                     placeholder="Jane Eyre"
+                    id="name"
                     name="name"
                     value={name}
                     onChange={handleChange}
                 />
             </div>
             <div className="form-row">
-                <label>Phone</label>
+                <label for="phone">Phone</label>
                 <input
                     type="tel"
                     placeholder="5141234567"
+                    id="phone"
                     name="phone"
                     value={phone}
                     onChange={handleChange}
                 />
             </div>
             <div className="form-row">
-                <label>Email</label>
+                <label for="email">Email</label>
                 <input
                     type="email"
                     placeholder="jane.eyre@hotmail.com"
+                    id="email"
                     name="email"
                     value={email}
                     onChange={handleChange}
                 />
             </div>
             <div className="form-row">
-                <label>Location</label>
+                <label for="location">Location</label>
                 <input
                     type="text"
                     placeholder="Montreal, Quebec"
+                    id="location"
                     name="location"
                     value={location}
                     onChange={handleChange}
                 />
             </div>
             <div className="form-row">
-                <label>Link</label>
+                <label for="link">Link</label>
                 <input
                     type="url"
                     placeholder="https://www.link-to-portfolio.com"
+                    id="link"
                     name="link"
                     value={link}
                     onChange={handleChange}
                 />
             </div>
             <div className="form-row">
-                <label>Summary</label>
+                <label for="summary">Summary</label>
                 <textarea
                     placeholder="Please provide a brief professional summary..."
+                    id="summary"
                     name="summary"
                     value={summary}
                     onChange={handleChange}

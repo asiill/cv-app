ea20c6f24b51ccb6e27d01245bdd1b32bff3d87c
asil <9745019+asiill@users.noreply.github.com>
2024-05-08 16:08:15 -0400

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useRef } from "react";
 import Header from "./components/Header.jsx";
 import Content from "./components/Content.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App() {
   return (
     <>
       <Header />
       <Content />
+      <Footer />
     </>
   );
 }

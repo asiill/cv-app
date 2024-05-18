0e9d08bf8335ce84576fb4af9f2f19f974aca5c6
asil <9745019+asiill@users.noreply.github.com>
2024-05-18 14:18:49 -0400

    Change title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import "../styles/header.css";
 export default function Header() {
     return (
         <header id="cv-app-header">
-            <h1>CV GENERATOR</h1>
+            <h1>CV CREATOR</h1>
         </header>
     )
 }
